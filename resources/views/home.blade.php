224bfc6b00888b0e0bd9dc7775ff0b663189ae05
Antoine <ant.guillard@gmail.com>
2020-11-02 22:44:08 +0100

    img conf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 
 <div class="container d-flex justify-content-center">
+    <img class="conf conf-left" src="{{ asset('/assets/conf.png') }}">
     <h2>Bravo, tu es connecté =D</h2>
+    <img class="conf conf-right" src="{{ asset('/assets/conf.png') }}">
 </div>
 
 <div class="container">
